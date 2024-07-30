95e0bb89f3aea89a2eeef5a5cfc513dba37e8f58
GUNJAN BORAD <90950505+Gunjan216@users.noreply.github.com>
2024-07-30 13:01:21 +0000

    operation

---
+++ Basic_01/04_operation.js
@@ -0,0 +1,19 @@
+let value =3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%2);
+// console.log(2/2);
+// console.log(2**2);
+
+let str1 ="hello"
+let str2 ="gunjan"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 3)
+console.log(3 + 3 + "1")
+console.log("1" + 3 +3)
