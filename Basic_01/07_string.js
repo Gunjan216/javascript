0f150303ac8f975f3b674330771659d0b20df17e
GUNJAN BORAD <90950505+Gunjan216@users.noreply.github.com>
2024-07-30 13:50:50 +0000

    Strings

---
+++ Basic_01/07_string.js
@@ -0,0 +1,9 @@
+const name = "gunjan"
+const repcount = 3
+// console.log(name + repcount + "helloworld");
+console.log(`hello my name is ${name.toLocaleUpperCase()} and my repo count is ${repcount}`)
+const gameName = new String("Gunjan")
+console.log(gameName.length)
+console.log(gameName.toLocaleUpperCase())
+// string methods
+// touppercase , tolowercase , charAt , trim , indexof , substr , sup , slice , replace , includes , split
