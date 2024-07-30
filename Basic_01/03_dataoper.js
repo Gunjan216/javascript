b7b40a8cd1907b8c3c71b615c48bdbf7da291278
GUNJAN BORAD <90950505+Gunjan216@users.noreply.github.com>
2024-07-30 12:47:52 +0000

    conversiondatatype

---
+++ Basic_01/03_dataoper.js
@@ -0,0 +1,16 @@
+let score = "33abc"
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NAN
+// true => 1 ; false =>0 
+// null => 0
+// undefined =>NAN
+
+// Boolean 
+// 1 => true ; 0 => false
+//  "" =>false
+// "gunjan" =>true
