8f7527831a4258623419ef10d56ba25fd0dd1753
GUNJAN BORAD <90950505+Gunjan216@users.noreply.github.com>
2024-07-30 13:24:31 +0000

    datatypesummery

---
+++ Basic_01/06_datasummery.js
@@ -0,0 +1,19 @@
+//  primitive datatype
+// 7 types : String , Number , Null , Boolean , Undefiend , Symbol , BigInt
+
+
+
+// non-primitive datatypes
+// Array , object , function 
+const hero = ["gunjan" , "borad" , "news"]
+console.log(hero[1])
+const students = {
+    name: "gunjan",
+    id : 1,
+    City :" surat"
+}
+ console.log(students.City)
+
+const add = function(){
+    console.log("hello world")
+}
