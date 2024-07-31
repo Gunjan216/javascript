87b5e6f67f9ac13878e744c836a9f4d6d04b1f5a
GUNJAN BORAD <90950505+Gunjan216@users.noreply.github.com>
2024-07-31 10:20:48 +0000

    number&math

---
+++ Basic_01/08_number-math.js
@@ -0,0 +1,22 @@
+const score = 300
+const balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+// console.log(score)
+
+// const othernumber = 123.456
+// console.log(othernumber.toPrecision(4));
+const india = 1000000
+// console.log(india.toLocaleString('en-IN'));
+// math in javascript
+
+console.log(Math.abs(4))
+//  .abs , .round , .ceil ,floor , random , .min , .max
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const min=10
+const max =20
+
+console.log(Math.floor(Math.random() * (max -min +1) + min))
