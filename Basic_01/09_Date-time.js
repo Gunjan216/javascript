6175e243544637488f14c154052b59a248e2c879
GUNJAN BORAD <90950505+Gunjan216@users.noreply.github.com>
2024-07-31 10:34:49 +0000

    date-time

---
+++ Basic_01/09_Date-time.js
@@ -0,0 +1,10 @@
+// dates - types object
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+
+let newdate = new Date(2003 , 5 ,21)
+console.log(newdate.toDateString());
+
+let myTime = Date.now()
+console.log(Date.now());
